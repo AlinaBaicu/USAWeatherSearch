41f94f193df60f186e0fb5f529c7575747d61574
Alina <alinabaicu93@gmail.com>
2018-06-06 08:02:28 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.static(__dirname));
-app.get('*',(req, res) =>{
- res.sendFile(path.resolve(__dirname,'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 app.listen(port);
-console.log('Server has started');
+console.log("Server has started");
